ded38e5bdc6368c34b4ed46b69f9539da55d8877
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-02-03 16:32:17 +0100

    Create the homepage style website

---
+++ src/components/homepage/HomepageStyled.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const MainContent = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  height: 8vh;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const FirstTitle = styled.h1`
+  font-size: 3em;
+  color: white;
+  text-shadow: 4px 4px 2px black;
+  padding: 0.5em;
+`;
+
+export const SiteName = styled.h1`
+  font-size: 3em;
+  color: black;
+  text-shadow: 4px 4px 2px #5d5147;
+  padding: 0.5em;
+`;
+
+export const Logo = styled.img`
+  height: 15vh;
+  width: auto;
+  box-shadow: 0px 0px 24px white;
+  background: linear-gradient(
+    90deg,
+    rgba(84, 203, 169, 0.6) 50%,
+    rgba(0, 0, 0, 0.1) 20%
+  );
+  border: 1px solid white;
+  padding: 0.5em;
+  border-radius: 15px;
+`;
+
+export const LogoAndSiteName = styled.div`
+  height: min-content;
+  width: auto;
+  display: flex;
+  align-items: center;
+`;
+
+export const Cartridge = styled.div`
+  height: 35vh;
+  width: 50vw;
+  background: #e2764e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 3em;
+  box-shadow: 4px 4px 12px #5d5147;
+`;
+
+export const Title = styled.h1`
+  font-size: 2em;
+  color: white;
+  text-shadow: 4px 4px 2px black;
+  padding: 0.5em;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 1.5em;
+  color: black;
+  text-shadow: 2px 2px 2px #5d5147;
+  padding: 1em;
+  line-height: 1.5em;
+`;
+
+export const Button = styled.div`
+  width: 10vw;
+  height: 6.5vh;
+  background: #cf582c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-shadow: 4px 4px 5px black;
+  box-shadow: 4px 4px 5px black;
+  margin: 1.5em;
+`;
