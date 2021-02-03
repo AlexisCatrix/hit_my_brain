92685cbc49232bdd44a9e8c25fa59f37e34ee5b8
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-02-03 20:01:44 +0100

    Add style for SignIn component and creation of form to sign in

--- src/components/signIn/SignInStyled.js
+++ src/components/signIn/SignInStyled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { ArrowBack } from "@styled-icons/boxicons-regular/ArrowBack";
 
 export const MainContent = styled.div`
   height: 100vh;
@@ -7,4 +8,39 @@ export const MainContent = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const BackIcon = styled(ArrowBack)`
+  height: inherit;
+  width: auto;
+`;
+
+export const Form = styled.form`
+  width: 30vw;
+  height: 50vh;
+  background: #e2764e;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 3em;
+  box-shadow: 4px 4px 12px #5d5147;
+`;
+
+export const LabelAndInput = styled.div`
+  width: 20vw;
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin: auto auto;
+`;
+
+export const Label = styled.label`
+  font-size: 1.25em;
+  margin: 1.5em 0;
+`;
+
+export const Input = styled.input`
+width: 18vw;
 `;
