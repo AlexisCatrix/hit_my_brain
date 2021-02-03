9b0c6457c1bb53440789d48916f5f0de744ba6ae
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-02-03 16:28:07 +0100

    Create Homepage component and initialize SignIn component, use react-router-dom to switch link between components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Homepage from "./components/homepage/Homepage";
+import SignIn from "./components/signIn/SignIn";
+
+import { Reset } from "styled-reset";
+import { Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Reset />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/SignIn" component={SignIn} />
+        <Homepage />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
