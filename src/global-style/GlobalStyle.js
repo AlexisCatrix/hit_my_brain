56e7195b56eecbd1fbb473fffd30ca79ad5c6070
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-02-03 16:35:13 +0100

    Create global style for app

---
+++ src/global-style/GlobalStyle.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const GlobalStyle = styled.body`
+  font-family: "Peralta", cursive;
+  background: linear-gradient(360deg, #da4040 55%, #ff823b);
+`;
