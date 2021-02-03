a4f3ca5aa100e5dbf6a36993607a2c33b887a8fa
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-02-03 16:31:45 +0100

    Create the homepage website

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Header,
+  MainContent,
+  FirstTitle,
+  SiteName,
+  LogoAndSiteName,
+  Logo,
+  Cartridge,
+  Title,
+  Paragraph,
+  Button,
+} from "../../components/homepage/HomepageStyled";
+import logo from "../../assets/pictures/logo_hitMyBrain.png";
+
+export default function Homepage() {
+  return (
+    <MainContent>
+      <Header>
+        <Link to="/SignIn">
+          <Button id="signIn">Sign in</Button>
+        </Link>
+      </Header>
+      <FirstTitle>Welcome to </FirstTitle>
+      <LogoAndSiteName>
+        <SiteName>Hit my brain</SiteName>
+        <Logo src={logo} />
+      </LogoAndSiteName>
+      <Cartridge>
+        <Title>What is “Hit my brain ?</Title>
+        <Paragraph>
+          This is a quiz game, many categories are available (Cinema, Music, Art
+          ...), you can even check your score with your friends. Pretty cool no
+          ;) ? So sign up and have fun!
+        </Paragraph>
+      </Cartridge>
+      <Button id="signUp">Sign up</Button>
+    </MainContent>
+  );
+}
