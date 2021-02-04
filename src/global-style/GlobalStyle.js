00c941445dbd56da921ae98f16104df05ed41f71
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-02-04 17:08:51 +0100

    Prettify

--- src/global-style/GlobalStyle.js
+++ src/global-style/GlobalStyle.js
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const GlobalStyle = styled.body`
   font-family: "Peralta", cursive;
   background: linear-gradient(360deg, #da4040 55%, #ff823b);
+
 `;
